998df531966a09a44cba63c1589bc96cbccc91fc
deokgoo <kkddgg1001@gmail.com>
2021-09-01 18:17:06 +0900

    feature: 노이즈 추가

--- src/script/oldVideoEffect.ts
+++ src/script/oldVideoEffect.ts
@@ -10,31 +10,67 @@ interface EffectControl {
 	animationRun: () => void;
 	animationStop: () => void;
 }
+
+const NOISE_IMG = 'http://localhost:8000/img/noise.jpg';
 class OldVideoEffect implements EffectControl {
-	private _img: HTMLDivElement;
+	private _target: HTMLDivElement;
+	private _noise: HTMLDivElement;
+	private _img: HTMLImageElement;
 	private _size: ImgSize;
 
-	private constructor(el: HTMLImageElement, size: ImgSize) {
-		this._img = el;
-		this._size = size;
+	private constructor(target: HTMLImageElement, imgUrl: string, size?: ImgSize) {
+		this._target = target;
+		if(size) this._size = size;
+		this.reSize();
+		this.createNoise();
+		this.defineImage(imgUrl);
 		this.filter('black');
+		this.render();
 	}
 
-	static createImg(el: HTMLImageElement, size: ImgSize) {
-		const oldVideoEffect = new OldVideoEffect(el, size);
+	static createImg({target, size, imgUrl}: {target: HTMLImageElement, imgUrl: string, size?: ImgSize}) {
+		const oldVideoEffect = new OldVideoEffect(target, imgUrl, size);
 		return oldVideoEffect;
 	}
 
-	reSize: (size: ImgSize) => void;
+	private createNoise() {
+		const noise: HTMLDivElement = document.createElement('div');
+		noise.classList.add('noise');
+		noise.style.backgroundImage = `url(${NOISE_IMG})`;
+		noise.style.width = `calc(${this._size.width}px*2)`;
+		noise.style.height = `calc(${this._size.height}px*2)`;
+		noise.style.position = 'absolute';
+		noise.style.left = `calc(-${this._size.width}px/2)`;
+		noise.style.top = `calc(-${this._size.height}px/2)`;
+		noise.style.animation = 'noiseAnimation 1s steps(1) infinite';
+		this._noise = noise;
+	}
+
+	reSize() {
+		this._target.style.position = 'relative';
+		this._target.style.width = `${this._size.width}px`;
+		this._target.style.height = `${this._size.height}px`;
+		this._target.style.overflow = 'hidden';
+	};
 	animationRun: () => void;
 	animationStop: () => void;
 
+	defineImage(imgUrl: string) {
+		this._img = document.createElement('img');
+		this._img.src = imgUrl;
+		this._img.style.opacity = `0.1`;
+	}
 
+	render() {
+		this._target.append(this._noise);
+		this._target.append(this._img);
+	}
 
 	filter(kind: Filter) {
 		switch(kind) {
 			case 'black': {
-				this._img.setAttribute('style', 'filter: grayscale(100%);');
+				this._img.setAttribute('style',
+				'filter: grayscale(100%); opacity: 0.7; width: 100%; height: 100%;');
 				break;
 			}
 			case 'white': {
